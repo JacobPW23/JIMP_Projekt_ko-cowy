f03bcdf95f7a23bc783a843c6ac22b8a75eff787
Jakub Próchnicki <jakub.prochnicki.stud@pw.edu.pl>
2024-01-04 09:15:43 +0100

    Wprowadzenie poprawek błędów przejścia mrówki w funkcji przejscie

--- src/mrowka.c
+++ src/mrowka.c
@@ -26,7 +26,7 @@ if(*obecne_pole_na_mapie=='b')
 
 
 		case 'N': stan->zwrot='E';*obecne_pole_na_mapie='c';if(*nr_kol==wierszy_na_mapie-1)  return 1; else *nr_kol++; break;
-		case 'E': stan->zwrot='S';*obecne_pole_na_mapie='c';if(*nr_kol==kolumn_na_mapie-1) return 1; else *nr_wier++; break;
+		case 'E': stan->zwrot='S';*obecne_pole_na_mapie='c';if(*nr_wier==wierszy_na_mapie-1) return 1; else *nr_wier++; break;
 		case 'S': stan->zwrot='W';*obecne_pole_na_mapie='c';if(*nr_kol==0) return 1; else *nr_kol--;break;
 		case 'W': stan->zwrot='N';*obecne_pole_na_mapie='c';if(*nr_wier==0) return 1 ;else*nr_wier--;break;
 
@@ -39,10 +39,10 @@ if(*obecne_pole_na_mapie=='c')
        {
 
 
-	        case 'N': stan->zwrot='W';*obecne_pole_na_mapie='b';if(*nr_kol==0)  return 1; else *nr_kol--;break;
+	       case 'N': stan->zwrot='W';*obecne_pole_na_mapie='b';if(*nr_kol==0)  return 1; else *nr_kol--;break;
 	        case 'E': stan->zwrot='N';*obecne_pole_na_mapie='b';if(*nr_wier==0) return 1; else *nr_wier--;break;
-		case 'S': stan->zwrot='E';*obecne_pole_na_mapie='b';if(*nr_kol==wierszy_na_mapie-1) return 1; else *nr_kol++; break;
-		case 'W': stan->zwrot='S';*obecne_pole_na_mapie='b';if(*nr_kol==kolumn_na_mapie-1) return 1; else *nr_wier++; break;
+		case 'S': stan->zwrot='E';*obecne_pole_na_mapie='b';if(*nr_kol==kolumn_na_mapie-1) return 1; else *nr_kol++; break;
+		case 'W': stan->zwrot='S';*obecne_pole_na_mapie='b';if(*nr_wier==wierszy_na_mapie-1) return 1; else *nr_wier++; break;
 
         }
 }
